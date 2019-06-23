e5beb479a4b9c51484bf5081da3db5b6f7a2ed5b
markroche32 <markroche32@gmail.com>
2019-06-23 20:37:13 +0100

    Add files via upload

---
+++ UnitTestProject1/WebDisclosureAutomationTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class WebDisclosureAutomationTests
+    {
+        private TestContext testContextInstance;
+        private IWebDriver driver;
+        
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+
+        [TestInitialize()]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.Manage().Window.Maximize();      
+            driver.Navigate().GoToUrl("https://viewpoint.glasslewis.com/WD/?siteId=DemoClient");
+          
+        }
+
+        [TestMethod]
+        public void WD_UpdateCounrtyShouldDisplayOnlyThatCountrysMeetingResults()
+        {
+            var country = "Belgium";
+            new WebDisclosureModel(driver).CountryCheckBox(country).Click(driver);
+            new WebDisclosureModel(driver).Update.Click(driver);
+            new WebDisclosureModel(driver).VerifyAllCountyColumnsAreContainCountry(country);     
+        }
+
+
+        [TestMethod]
+        public void WD_ClickCompanyNameShouldNavigateToCompanysVoteCardPage()
+        {           
+            var company = "Activision Blizzard Inc";
+            bool found = false;
+
+            while (!found)
+            {
+                try
+                {
+                    new WebDisclosureModel(driver).CompanyName(company).Click(driver);
+                    found = true;
+                    Assert.AreEqual(company, new WebDisclosureModel(driver).CompanyVoteCardPage(company).GetText(driver));
+
+                }
+                catch (NoSuchElementException ex)
+                {
+                    //Go Right to Next Page While Possible and Repeat above logic in try block
+                    if (!new WebDisclosureModel(driver).GoRight.GetAttribute("class").Contains("disabled"))
+                    {
+                        new WebDisclosureModel(driver).GoRight.Click(driver);
+                    }
+                    else
+                    {
+                        Assert.Fail("Company : " + company + " Is Not Available For Selection");
+                    }
+                    
+                }
+            }
+        
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+
+    }
+}
---
+++ UnitTestProject1/WebDisclosureModel.cs
@@ -0,0 +1,81 @@
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    class WebDisclosureModel
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.XPath, Using = @"//label[@for='Belgium-cb-CountryFilter']")]
+        public IWebElement BelgiumCheckbox { get; set; }
+
+        [FindsBy(How = How.XPath, Using = @"//*[@id='btn-update'][@class='btnMain btnSize']")]
+        public IWebElement Update { get; set; }
+
+        [FindsBy(How = How.XPath, Using = @"//table//tbody//tr//td[4]")]
+        public IList<IWebElement> CountryColumn { get; set; }
+
+        [FindsBy(How = How.XPath, Using = @"//a[@title='Go to the next page']")]
+        public IWebElement GoRight { get; set; }
+
+
+        public WebDisclosureModel(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+
+        public IWebElement CountryCheckBox(string country)
+        {
+
+            return driver.FindElement(By.XPath("//label[@for='" + country + "-cb-CountryFilter']"));
+        }
+
+        public IWebElement CompanyName(string company)
+        {
+
+            return driver.FindElement(By.XPath("//a[contains(text(),'" + company + "')]"));
+        }
+
+        public IWebElement CompanyVoteCardPage(string company)
+        {
+
+            return driver.FindElement(By.XPath("//span[@id='detail-issuer-name' and text()='" + company + "']"));
+        }
+
+        public void VerifyAllCountyColumnsAreContainCountry(string country)
+        {
+            int count = 0;
+
+            //Initialize All Country Column Elements
+            IList<IWebElement> countryColumnsElem = new WebDisclosureModel(driver).CountryColumn;
+
+            //Iterate All Country Column Elements
+            for (int i=0; i < countryColumnsElem.Count;i++) 
+             {
+                string title = countryColumnsElem[i].GetText(driver);
+                Assert.AreEqual(title, country);
+                count++;
+
+                //When Country Columns >= 50, Recursively perform same logic above 
+                //Naviage to Next Page if Possible ; case where Country Columns == 50
+                if (count == 49 && !new WebDisclosureModel(driver).GoRight.GetAttribute("class").Contains("disabled"))
+                {
+                    new WebDisclosureModel(driver).GoRight.Click(driver);
+                    VerifyAllCountyColumnsAreContainCountry(country);
+                }
+            }
+
+        }
+
+    }
+
+}
